9b0aa11af5b4f52361d034414d3bb6d6628b825c
James Ware <jazz642@gmail.com>
2018-12-31 15:27:48 -0500

    Created post list view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
+
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('-published_date')
+    data = {'posts': posts}
+    return render(request, 'blog/post_list.html', data)
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # this will check the path first, then it will check inside the urls file in blog
+    path('', include('blog.urls')),
 ]
