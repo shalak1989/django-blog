977301710ec41f9f81351ff3570e30cc6e76c6bb
James Ware <jazz642@gmail.com>
2018-12-31 16:23:16 -0500

    added in post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 
@@ -8,4 +8,10 @@ def post_list(request):
     posts = Post.objects.all().order_by('-published_date')
     data = {'posts': posts}
     return render(request, 'blog/post_list.html', data)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    data = {'post': post}
+    return render(request, 'blog/post_detail.html', data)
 
